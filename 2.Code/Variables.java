6c0adeeed4ef26f129c66bbdc8e1c0a8224e4809
ramitdour <ramit.dour@gmail.com>
2022-10-29 02:26:48 +0530

    Variables and DataTypes

---
+++ 2.Code/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args){
+        System.out.println("hello Variables !!!");
+
+//        Decleration statement  -  Assignment statement
+        int myFirstNumber =  5; // range
+        System.out.println(myFirstNumber);
+    }
+}
