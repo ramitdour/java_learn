640cb0ce7c0b07f4eb1b3fd9bfda6b3d187c9625
ramitdour <ramit.dour@gmail.com>
2022-10-29 15:19:14 +0530

    Variables and DataTypes Strings

--- 2.Code/DataTypes.java
+++ 2.Code/DataTypes.java
@@ -4,14 +4,24 @@ public class DataTypes {
 
 
         int myNum = 5;               // Integer (whole number)
+
+        myNum = Integer.MAX_VALUE;
+        myNum = Integer.MIN_VALUE;
+
+        myNum = 2_147_483_646; // You can put underscore to make it readable
+
         float myFloatNum = 5.99f;    // Floating point number
+        double myDoubleNumm = 5.99;    // Double Floating point number without d , works
+        double myDoubleNum = 5.99d;    // Double Floating point number
         char myLetter = 'D';         // Character
+        char myChar = '\u0044';         // Character
         boolean myBool = true;       // Boolean
         String myText = "Hello";     // String
 
         System.out.println(myNum);
         System.out.println(myFloatNum);
         System.out.println(myLetter);
+        System.out.println(myChar);
         System.out.println(myBool);
         System.out.println(myText);
 
@@ -28,6 +38,18 @@ public class DataTypes {
 //        double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
 //        boolean	1 bit	Stores true or false values
 //        char	    2 bytes	Stores a single character/letter or ASCII values
+
+
+        byte myMinByteVal = Byte.MIN_VALUE;
+
+        System.out.println(myMinByteVal);
+
+//        Casting
+
+        int myTotal = (myMinByteVal / 2);
+
+//        byte myNewByteVal = (myMinByteVal / 2); // ERROR beacause of 2 in int
+        byte myNewByteVal = (byte) (myMinByteVal / 2); // Casting by (byte)
 
 
     }
---
+++ 2.Code/NonPrimitiveDataType.java
@@ -0,0 +1,37 @@
+public class NonPrimitiveDataType {
+    public static void main(String[] args) {
+        System.out.println("NonPrimitiveDataType");
+
+
+        // String is immutable in JAVA , new string is created old one is discarded.
+        String myFName = "Ramit";
+        myFName = myFName + " Hero \u00A9 ";
+        System.out.println(myFName + "Dour");
+
+        String num_str = "225.55";
+
+        String a = "4" ;
+        int b = 5 ;
+        System.out.println(a + b); //45 String
+        System.out.println(b + a); //54 String
+
+//        System.out.println((int)a + b); //ERROR java.lang.String cannot be converted to int
+//        System.out.println(b + (int)a); //ERROR java.lang.String cannot be converted to int
+
+//        System.out.println((Integer)a + b); // ERROR ava: incompatible types: java.lang.String cannot be converted to java.lang.Integer
+//        System.out.println(b + (Integer)a); // ERROR ava: incompatible types: java.lang.String cannot be converted to java.lang.Integer
+
+        System.out.println( Integer.valueOf(a) + b); //9
+        System.out.println(b +  Integer.valueOf(a)); //9
+
+        //better
+        System.out.println( Integer.parseInt(a) + b); //9
+        System.out.println(b +  Integer.parseInt(a)); //9
+
+
+        // NEXT CONCEPT:StringBuffer
+
+
+
+    }
+}
