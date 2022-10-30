5c6c45c5a669034e887f93314a4c3f1f5e0cdff6
ramitdour <ramit.dour@gmail.com>
2022-10-30 17:26:11 +0530

    MegaBytesConverter

---
+++ 2.Code/Part2/MegaBytesConverter.java
@@ -0,0 +1,19 @@
+package Part2;
+
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if(kiloBytes < 0 ){
+            System.out.println("Invalid Value");
+        }else{
+            int YY = kiloBytes / 1024;
+            int ZZ = kiloBytes % 1024;
+            if (ZZ == 0){
+                System.out.println(""+kiloBytes+" KB = "+ YY +" MB");
+            }else {
+                System.out.println(""+kiloBytes+" KB = "+ YY +" MB and "+ ZZ +" KB");
+            }
+        }
+    }
+
+}
---
+++ 2.Code/Part2/SpeedConverter.java
@@ -0,0 +1,20 @@
+package Part2;
+
+public class SpeedConverter {
+    // write your code here
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609d);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println("" + kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
