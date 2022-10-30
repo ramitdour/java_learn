fa7ddc8c88dcf9c64834c2c23ee90d144d3ee8e3
ramitdour <ramit.dour@gmail.com>
2022-10-30 18:44:57 +0530

    IntEqualityPrinter

---
+++ 2.Code/Part2/IntEqualityPrinter.java
@@ -0,0 +1,30 @@
+package Part2;
+
+public class IntEqualityPrinter {
+    // write your code here
+    public static void printEqual(int a, int b ,int c){
+        if(a < 0 || b < 0 || c < 0 ){
+            System.out.println("Invalid Value");
+        }else{
+            if (a == b){
+                if (b == c) {
+                    System.out.println("All numbers are equal");
+                } else {
+                    System.out.println("Neither all are equal or different");
+                }
+
+            }else{
+                if (b == c){
+                    System.out.println("Neither all are equal or different");
+                }else{
+                    if(a == c){
+                        System.out.println("Neither all are equal or different");
+                    }
+                    else{
+                        System.out.println("All numbers are different");
+                    }
+                }
+            }
+        }
+    }
+}
