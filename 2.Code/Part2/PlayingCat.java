c8f57fee3135f4e08482904786a81a5d34923465
ramitdour <ramit.dour@gmail.com>
2022-10-30 18:51:24 +0530

    PlayingCat

---
+++ 2.Code/Part2/PlayingCat.java
@@ -0,0 +1,22 @@
+package Part2;
+
+public class PlayingCat {
+    // write your code here
+    public static boolean isCatPlaying(boolean summer ,int temperature ){
+        if ( temperature < 25  || temperature > 45){
+            return false;
+        }else{
+            if (!summer){
+                if (temperature > 35 ){
+                    return false;
+                }else{
+                    return true;
+                }
+            }else{
+                return true;
+            }
+        }
+
+    }
+
+}
