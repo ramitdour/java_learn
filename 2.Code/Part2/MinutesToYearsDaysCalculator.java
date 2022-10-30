dc75ad012880b1d81ecb9c79f071a53487315ed1
ramitdour <ramit.dour@gmail.com>
2022-10-30 18:37:37 +0530

    MinutesToYearsDaysCalculator

---
+++ 2.Code/Part2/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,16 @@
+package Part2;
+
+public class MinutesToYearsDaysCalculator {
+    // write your code here
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        }else{
+            int dayMins = 24 * 60 ;
+            int yearMins = dayMins * 365 ;
+            long YY = minutes / yearMins;
+            long ZZ = (minutes % yearMins) / dayMins;
+            System.out.println(""+minutes+" min = "+YY+" y and "+ZZ+" d");
+        }
+    }
+}
