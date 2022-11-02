9ce33d60ebe909a3eea4c3475550f50cafa76df8
ramitdour <ramit.dour@gmail.com>
2022-11-02 18:32:42 +0530

    Person Class

--- 2.Code/Part3/examples/Main.java
+++ 2.Code/Part3/examples/Main.java
@@ -14,5 +14,20 @@ public class Main {
         System.out.println("multiply= " + calculator.getMultiplicationResult());
         System.out.println("divide= " + calculator.getDivisionResult());
 
+        System.out.println("+++++++++++++++++++++++++");
+
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+
     }
 }
