ddf0938e28c30bec2640b79e02f4d5c00dca67bc
ramitdour <ramit.dour@gmail.com>
2022-11-02 13:53:21 +0530

    LastDigitChecker

---
+++ 2.Code/Part2/LastDigitChecker.java
@@ -0,0 +1,25 @@
+package Part2;
+
+public class LastDigitChecker {
+    // write your code here
+    public static boolean hasSameLastDigit(int a ,int b , int c){
+        if( (a < 10) || (a > 1000) || (b < 10) || (b > 1000) || (c < 10) || (c > 1000)){
+            return false ;
+
+        }
+        int x =  (a%10) ;
+        int y = ( b%10) ;
+        int z =  (c%10);
+
+
+        if ( x ==y || y == z || z == x ){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValid(int n){
+        if ( (n < 10 )|| (n > 1000)) { return false;}
+        return true;
+    }
+}
