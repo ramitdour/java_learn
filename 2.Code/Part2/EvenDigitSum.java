c8fc238d727dce89b91e8fd8e0c09f12d4c509c7
ramitdour <ramit.dour@gmail.com>
2022-11-02 13:36:39 +0530

    EvenDigitSum

---
+++ 2.Code/Part2/EvenDigitSum.java
@@ -0,0 +1,19 @@
+package Part2;
+
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number){
+        if (number < 0) {
+            return  -1;
+        }
+        int sum = 0;
+        while (number > 0){
+            int rem =  number % 10 ;
+            if(rem % 2 == 0){
+                sum += rem ;
+            }
+            number = number / 10;
+        }
+
+        return sum;
+    }
+}
