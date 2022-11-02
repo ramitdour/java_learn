e8c964f7226837bbe1b625fdf2885085fe67ec6c
ramitdour <ramit.dour@gmail.com>
2022-11-02 13:32:00 +0530

    FirstLastDigitSum

---
+++ 2.Code/Part2/FirstLastDigitSum.java
@@ -0,0 +1,18 @@
+package Part2;
+
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number){
+        if(number < 0){
+            return -1;
+        }
+
+        int sum = number % 10;
+
+        while (number > 9){
+            number = number / 10;
+        }
+
+        sum += number ;
+        return  sum;
+    }
+}
