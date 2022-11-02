def180735b2d1c9a55244b559e54da197e6d68be
ramitdour <ramit.dour@gmail.com>
2022-11-02 14:07:42 +0530

    FactorPrinter

---
+++ 2.Code/Part2/FactorPrinter.java
@@ -0,0 +1,20 @@
+package Part2;
+
+public class FactorPrinter {
+    // write your code here
+    public static void printFactors(int number){
+
+        String s = "1";
+        if( number < 1){
+            System.out.println("Invalid Value");
+        }else{
+            for(int i = 2 ; i <= number;i++ ){
+                if(number % i == 0){
+                    s = s + " " + i;
+                }
+            }
+            System.out.println(s);
+        }
+
+    }
+}
