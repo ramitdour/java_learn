382d9129564050a2f77a499df6bb307660d7d4f8
ramitdour <ramit.dour@gmail.com>
2022-11-02 13:42:47 +0530

    SharedDigit

---
+++ 2.Code/Part2/SharedDigit.java
@@ -0,0 +1,20 @@
+package Part2;
+
+public class SharedDigit {
+    // write your code here
+    public static boolean hasSharedDigit(int x , int y){
+        if( (x < 10) || (x > 99) || (y < 10) || (y > 99)){
+            return false ;}
+
+        int x_a = x % 10 ;
+        x /= 10 ;
+
+        int y_a = y % 10 ;
+        y /= 10;
+
+        if( (x == y) ||(x == y_a) ||(x_a == y) ||(x_a == y_a) ){
+            return true;
+        }
+        return false;
+    }
+}
