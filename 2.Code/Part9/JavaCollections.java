f48fbfd6b110998a953fd92127d1f09aed46ff4c
ramitdour <ramit.dour@gmail.com>
2022-11-05 18:22:30 +0530

    Java Collections

---
+++ 2.Code/Part9/JavaCollections.java
@@ -0,0 +1,7 @@
+package Part9;
+
+public class JavaCollections {
+    public static void main(String[] args) {
+        System.out.println("Java Collections");
+    }
+}
---
+++ 2.Code/Part9/examples/JCEx.java
@@ -0,0 +1,7 @@
+package Part9.examples;
+
+public class JCEx {
+    public static void main(String[] args) {
+        System.out.println("Java Collections examples");
+    }
+}
