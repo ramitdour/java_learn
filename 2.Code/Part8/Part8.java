9bb18c829ad89223d56b3083c21d70a4940f09e7
ramitdour <ramit.dour@gmail.com>
2022-11-05 15:57:47 +0530

    Packages

---
+++ 2.Code/Part8/Part8.java
@@ -0,0 +1,7 @@
+package Part8;
+
+public class Part8 {
+    public static void main(String[] args) {
+        System.out.println("naming conventions ,packages ,scope ,access modifiers ,static ,final");
+    }
+}
---
+++ 2.Code/Part8/example/exampleMain.java
@@ -0,0 +1,4 @@
+package Part8.example;
+
+public class exampleMain {
+}
